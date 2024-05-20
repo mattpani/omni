04e119c4ea34f0a6b1c748e43bac1422a8704e9d
mattpani <matuspanik6@gmail.com>
2024-05-20 16:40:53 +0200

    Implementation of migration with flyway - configuration for flyway classpath,datasource, connection parameters -sql init for tables and constraints - clean up of gradle files

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ Application that is supposed to gather and unite information from world organiza
 3. Using JDBC - Dao pattern
 4. Junit testing 
 5. PostgresDB - dockerized 
-
+6. Flyway migration
 
 Modules 
 1. Persistence 
@@ -16,3 +16,12 @@ Modules
     - using JDBC to access, manipulate and save data
     - handle cases - for example where there is not required parameter 
    
+
+
+USED material and documentation
+- https://docs.spring.io/spring-boot/docs/2.1.x/reference/html/howto-data-access.html 
+- https://documentation.red-gate.com/fd/flyway-cli-and-api-183306238.html
+- https://www.digitalocean.com/community/tutorials/dao-design-patternjd
+- https://spring.io/guides/gs/relational-data-access
+- https://sabledhiraj.medium.com/spring-boot-multi-module-project-b984888a35d9
+- https://bootify.io/multi-module/best-practices-for-spring-boot-multi-module.html
---
+++ omni-rest/src/main/resources/db/migration/V1__create_tables.sql
@@ -0,0 +1,55 @@
+-- this the initialization using flyway for database that can be run from docker compose file
+-- it is necessary to use the standard naming conventions for sql scripts
+-- flyway_schema_history is created automatically and is used for tracking the changes in migration schema for example by checksum
+-- great tool over all, for easy project transfer
+
+-- Create the Person table
+CREATE TABLE Person (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(100),
+    last_name VARCHAR(100),
+    date_of_birth DATE
+);
+
+-- Create the Contact table for person and organization
+CREATE TABLE Contact (
+    id SERIAL PRIMARY KEY,
+    phone_number VARCHAR(15),
+    email VARCHAR(100),
+    address VARCHAR(255),
+    person_id INT,
+    organization_id INT
+);
+
+-- Create the Organization table
+CREATE TABLE Organization (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100),
+    contact_id INT
+);
+
+-- Create the OrganizationPerson relation many to many
+CREATE TABLE Organization_person (
+    organization_id INT,
+    person_id INT,
+    PRIMARY KEY (organization_id, person_id)
+);
+
+-- Adding constraint to Organization
+ALTER TABLE Organization
+ADD CONSTRAINT fk_organization_contact
+FOREIGN KEY (contact_id) REFERENCES Contact(id);
+
+-- Adding constraint to Contact
+ALTER TABLE Contact
+ADD CONSTRAINT fk_contact_person
+FOREIGN KEY (person_id) REFERENCES Person(id),
+ADD CONSTRAINT fk_contact_organization
+FOREIGN KEY (organization_id) REFERENCES Organization(id);
+
+-- Add constraints to OrganizationPerson
+ALTER TABLE Organization_person
+ADD CONSTRAINT fk_organization_person
+FOREIGN KEY (organization_id) REFERENCES Organization(id),
+ADD CONSTRAINT fk_person_organization_person
+FOREIGN KEY (person_id) REFERENCES Person(id);
