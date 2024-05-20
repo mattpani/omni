2046b51d4491f5bcac399d1ff2418e11a9122afe
mattpani <matuspanik6@gmail.com>
2024-05-20 16:51:26 +0200

    Basic spring-boot test for connection check to PostgresDB with validation of relations between objects, Implementing main class for test configuration

---
+++ omni-persistence/src/test/java/sk/matt/RelationTest.java
@@ -0,0 +1,50 @@
+package sk.matt;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import sk.matt.dao.impl.OrganizationDaoImpl;
+import sk.matt.dao.impl.PersonDaoImpl;
+import sk.matt.entity.Contact;
+import sk.matt.entity.Organization;
+import sk.matt.entity.Person;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class RelationTest {
+
+    @Autowired
+    private PersonDaoImpl personService;
+
+    @Autowired
+    private OrganizationDaoImpl organizationService;
+
+    @Test
+    public void testRelationsAndDatabase() {
+        Person person = new Person();
+        person.setFirstName("John");
+        person.setLastName("Doe");
+
+        Contact contact = new Contact();
+        contact.setAddress("123 Test St");
+        person.addContacts(contact);
+
+        Person savedPerson = personService.savePerson(person);
+
+        Organization org = new Organization();
+        org.setName("Test org");
+
+        Contact contOrg = new Contact();
+        contOrg.setAddress("123 Test Org");
+
+        org.setContact(contOrg);
+
+        assertNotNull(organizationService.saveOrganization(org));
+
+        assertNotNull(savedPerson);
+        assertEquals(person, savedPerson);
+    }
+
+}
