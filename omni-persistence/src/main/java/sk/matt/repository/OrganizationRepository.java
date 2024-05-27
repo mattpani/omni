106142d2f6d3be9b2eba4aca3095c655037b2b93
mattpani <matuspanik6@gmail.com>
2024-05-27 21:56:43 +0200

    Adding custom queries to get Person by Full name and Org by name

--- omni-persistence/src/main/java/sk/matt/repository/OrganizationRepository.java
+++ omni-persistence/src/main/java/sk/matt/repository/OrganizationRepository.java
@@ -1,10 +1,18 @@
 package sk.matt.repository;
 
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import sk.matt.entity.Organization;
+import sk.matt.entity.Person;
+
+import java.util.List;
 
 @Repository
 public interface OrganizationRepository extends CrudRepository<Organization, Long> {
+
+    @Query(  value = "SELECT * FROM organization ls where ls.name = :name")
+    List<Organization> getOrganizationByName(@Param("name") String name);
 }
 
--- omni-persistence/src/main/java/sk/matt/repository/PersonRepository.java
+++ omni-persistence/src/main/java/sk/matt/repository/PersonRepository.java
@@ -1,10 +1,16 @@
 package sk.matt.repository;
 
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import sk.matt.entity.Person;
+
+import java.util.List;
 
 @Repository
 public interface PersonRepository extends CrudRepository<Person, Long> {
 
+    @Query(  value = "SELECT * FROM person ls where ls.first_name = :firstName and ls.Last_name = :lastName")
+    List<Person> findPersonByFullName(@Param("firstName") String firstName,@Param("lastName") String lastName);
 }
